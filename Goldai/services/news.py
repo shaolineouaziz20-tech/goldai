a92e00e725774fe905801b81059ffea23e4709dc
shaolineouaziz20-tech <shaolineouaziz20@gmail.com>
2026-07-17 22:53:00 +0100

    Delete Goldai directory

--- Goldai/services/news.py
+++
@@ -1,5 +0,0 @@
-from services.news_provider import get_today_news
-
-
-def get_high_impact_news():
-    return get_today_news()
--- Goldai/services/news_engine.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def fetch_news():
-
-    # مؤقتاً
-    # غادي نربطوه بـ API الحقيقي من بعد
-
-    return []
